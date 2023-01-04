aca6bce653f46484131dc6151d06268a58026860
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2023-01-04 23:24:49 +0600

    Add files via upload

--- Explore Area Interface/exploreGUI.java
+++ Explore Area Interface/exploreGUI.java
@@ -1,67 +1,44 @@
-// import javax.print.attribute.standard.MediaSize.NA;
 import javax.swing.*;
-// import javax.swing.border.Border;
-// import javax.swing.border.TitledBorder;
-// import javax.swing.plaf.ColorUIResource;
-// import javax.swing.text.AbstractDocument.LeafElement;
-
 import java.awt.*;
 import java.awt.event.*;
-// import java.io.File;
-// import java.io.FileReader;
-// import java.io.IOException;
+
 public class exploreGUI implements ActionListener{
-    static JButton []buttons = new JButton[8];
-    JButton bookButton=new JButton();
     JFrame frame=new JFrame();
+    JButton buttons[] = new JButton[8];
+    JButton bookButton=new JButton();
+    JButton homeButton=new JButton();
 
-    exploreGUI(){
+    exploreGUI(){ 
 
-        
-        // JLabel imgJLabel=new JLabel(new ImageIcon("ex.png"));
-        JLabel imgJLabel=new JLabel(new ImageIcon("exl.png"));
+        JLabel imgJLabel=new JLabel(new ImageIcon("images/exl.png"));
         imgJLabel.setSize(1280,850);
         imgJLabel.setOpaque(false);
 
         JPanel panel = new JPanel();
         panel.setBounds(5,200,1280,850);
         panel.setLayout(null);
-        // panel.add(imgJLabel);
-
-        // panel.setLayout(new GridLayout(2,3));
+        
 
         int l=0;
         for(int i=0;i<4;i++){
             buttons[i]=new JButton();
             buttons[i].setBounds(10+315*i,330,310,250);
             buttons[i].addActionListener(this);
-            buttons[i].setForeground(new Color(0xFFFFFF));
             buttons[i].setBackground(new Color(0x292929));
             buttons[i].setOpaque(false);
             buttons[i].setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-
-            // buttons[i].setHorizontalTextPosition(JButton.CENTER);
-		    // buttons[i].setVerticalTextPosition(JButton.BOTTOM);
-            // buttons[i].setFont(new Font("Times new roman", Font.BOLD, 25));
-		    // buttons[i].setIconTextGap(20);
             buttons[i].setFocusable(false);
             panel.add(buttons[i]);
         }        
         for(int i=4;i<8;i++){
             buttons[i]=new JButton();
-            buttons[i].setBounds(10+310*l,330+250,310,250);
+            buttons[i].setBounds(10+315*l,330+250,310,250);
             buttons[i].setFocusable(false); 
             buttons[i].addActionListener(this);
-            buttons[i].setForeground(new Color(0xFFFFFF));
             buttons[i].setBackground(new Color(0x292929));
             buttons[i].setOpaque(false);
             buttons[i].setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-            // buttons[i].setHorizontalTextPosition(JButton.CENTER);
-		    // buttons[i].setVerticalTextPosition(JButton.BOTTOM);
-            // buttons[i].setFont(new Font("Times new roman", Font.BOLD, 25));
-		    // buttons[i].setIconTextGap(20);
-            
-            panel.add(buttons[i]);
+             panel.add(buttons[i]);
             l++;
         }
         
@@ -84,26 +61,29 @@ public class exploreGUI implements ActionListener{
         bookButton.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));       
         panel.add(bookButton);
        
+        
+        homeButton.setBounds(26,60,120,50);
+        homeButton.setFocusable(false); 
+        homeButton.addActionListener(this);
+        homeButton.setForeground(new Color(0xFFFFFF));
+        homeButton.setBackground(new Color(29,29,29,10));
+        homeButton.setOpaque(false);
+        homeButton.setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 0));       
+        panel.add(homeButton);
+
   
         panel.add(imgJLabel);
        
-        frame.setTitle("Exp");
-		frame.setSize(1295,760);
+        frame=new JFrame();
+        frame.setTitle("Explore");
+		frame.setBounds(300, 100,1295,860);
         frame.add(panel);
-
         frame.setMinimumSize(new Dimension(1295,860));
-            Toolkit tk=Toolkit.getDefaultToolkit();
-            int x=(int) tk.getScreenSize().getWidth();
-            int y=(int) tk.getScreenSize().getHeight();
-        frame.setLocation(x/2-1295/2, y/2-850/2);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setBackground(new Color(0xe8ab00));
-        frame.setIconImage(new ImageIcon("admin1.png").getImage());
+        frame.setIconImage(new ImageIcon("images/exIcon.png").getImage());
         frame.setResizable(false);
         frame.setVisible(true);
-        
-
-    
+              
     }
 
     public static void main(String[] args) {
@@ -111,44 +91,49 @@ public class exploreGUI implements ActionListener{
 
     }
 
-    // @Override
-    public void actionPerformed(ActionEvent e) {
+   
+    public void actionPerformed(ActionEvent e)  {
         if(e.getSource()==bookButton){
+            frame.setVisible(false);
+            new trainGUI();
+           }
+           
+        if(e.getSource()==homeButton){
             frame.setVisible(false);
            }
         if(e.getSource()==buttons[0]){
-            System.out.println("jhghj");
             frame.setVisible(false);
-            new expDhakaGUI();
+            new expDhaka("images/d1.png","images/d2.png","images/d3.png","images/d4.png","Dhaka");
         }
         if(e.getSource()==buttons[1]){
             frame.setVisible(false);
-            //    new ShovanSeatView(new JLabel("Shovan Seat View"),new ImageIcon("SHOVAN1.jpg"), new ImageIcon("SHOVAN2.jpg"), new ImageIcon("SHOVAN3.jpg"), new ImageIcon("SHOVAN4.jpg"));
+            new expKhulna("images/k1.png", "images/k2.png", "images/k3.png", "images/k4.png", "Khulna");
+
            }
         if(e.getSource()==buttons[2]){
             frame.setVisible(false);
-            //    new ShovanSeatView(new JLabel("Shovan Seat View"),new ImageIcon("SHOVAN1.jpg"), new ImageIcon("SHOVAN2.jpg"), new ImageIcon("SHOVAN3.jpg"), new ImageIcon("SHOVAN4.jpg"));
+            new expSylhet("images/s1.png", "images/s2.png", "images/s3.png", "images/s4.png", "Sylhet");
            }
         if(e.getSource()==buttons[3]){
             frame.setVisible(false);
-            //    new ShovanSeatView(new JLabel("Shovan Seat View"),new ImageIcon("SHOVAN1.jpg"), new ImageIcon("SHOVAN2.jpg"), new ImageIcon("SHOVAN3.jpg"), new ImageIcon("SHOVAN4.jpg"));
-           }
+            new expBarisal("images/b1.png", "images/b2.png", "images/b3.png", "images/b4.png", "Barisal");
+        }
         if(e.getSource()==buttons[4]){
             frame.setVisible(false);
-            //    new ShovanSeatView(new JLabel("Shovan Seat View"),new ImageIcon("SHOVAN1.jpg"), new ImageIcon("SHOVAN2.jpg"), new ImageIcon("SHOVAN3.jpg"), new ImageIcon("SHOVAN4.jpg"));
-           }
+            new expRajshahi("images/r1.png","images/r2.png","images/r3.png","images/r4.png","Rajshahi");
+        }
         if(e.getSource()==buttons[5]){
             frame.setVisible(false);
-            //    new ShovanSeatView(new JLabel("Shovan Seat View"),new ImageIcon("SHOVAN1.jpg"), new ImageIcon("SHOVAN2.jpg"), new ImageIcon("SHOVAN3.jpg"), new ImageIcon("SHOVAN4.jpg"));
+            new expRangpur("images/rng1.png", "images/rng2.png", "images/rng3.png", "images/rng4.png", "Rangpur");
            }
         if(e.getSource()==buttons[6]){
             frame.setVisible(false);
-            //    new ShovanSeatView(new JLabel("Shovan Seat View"),new ImageIcon("SHOVAN1.jpg"), new ImageIcon("SHOVAN2.jpg"), new ImageIcon("SHOVAN3.jpg"), new ImageIcon("SHOVAN4.jpg"));
+            new expChittagong("images/c1.png","images/c2.png","images/c3.png","images/c4.png","Chittagong");
            }
         if(e.getSource()==buttons[7]){
             frame.setVisible(false);
-            //    new ShovanSeatView(new JLabel("Shovan Seat View"),new ImageIcon("SHOVAN1.jpg"), new ImageIcon("SHOVAN2.jpg"), new ImageIcon("SHOVAN3.jpg"), new ImageIcon("SHOVAN4.jpg"));
-           }
+            new expMymensingh("images/m1.png", "images/m2.png", "images/m3.png", "images/m4.png", "Mymensingh");
+           } 
                
     }
 }
